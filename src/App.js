2cc0bc3789ac44a1590cce8d0765d7ff71c0de20
Koras02 <wjdgh0727@gmail.com>
2021-08-25 12:51:32 +0900

    최종

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
-import logo from './logo.svg';
+import React from 'react'
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {/* NODE_ENV로 개발서버 */}
+        {process.env.REACT_APP_MY_API_KEY}
       </header>
     </div>
   );
